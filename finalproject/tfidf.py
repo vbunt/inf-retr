e314aebed6b66e97d892954ea530d62c917dd7f7
vbunt <54185665+vbunt@users.noreply.github.com>
2022-10-18 17:27:10 +0300

    Update tfidf.py

--- finalproject/tfidf.py
+++ finalproject/tfidf.py
@@ -3,6 +3,8 @@ from scipy.sparse import load_npz
 from pickle import load
 from corpus import Corpus
 
+# initialize a search engine with tfidf
+
 
 class TfIdf:
 
@@ -17,10 +19,3 @@ class TfIdf:
         sim = cosine_similarity(vector, self.embeddings)
         names_sim = {self.documents[i]: sim[0][i] for i in range(len(self.documents))}
         return sorted(names_sim.keys(), key=lambda key: names_sim[key], reverse=True)[:5]
-
-
-# with open('big_dirty_answers.txt', 'r') as file:
-#     dirty_documents = file.read().split('\n')[:-1]
-# print(TfIdf(dirty_documents).search('возможно'))
-
-# print(len(dirty_documents))
