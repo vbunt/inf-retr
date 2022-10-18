b5dd5ae0f2da7649a12453b33e6a2b4a49a7b4de
vbunt <54185665+vbunt@users.noreply.github.com>
2022-10-18 17:26:24 +0300

    Update bm25.py

--- finalproject/bm25.py
+++ finalproject/bm25.py
@@ -3,6 +3,8 @@ from numpy import array, argsort
 from scipy.sparse import load_npz
 from pickle import load
 
+# initialize a search engine with BM25
+
 
 class BM25:
 
@@ -17,7 +19,3 @@ class BM25:
         scores = self.embeddings * query_count_vec.transpose()
         sorted_scores_indx = argsort(scores, axis=0)[::-1]
         return self.documents[sorted_scores_indx.ravel()][:5]
-
-# with open('dirty_answers.txt', 'r') as file:
-#     dirty_documents = file.read().split('\n')[:-1]
-# print(BM25(dirty_documents).search('возможно'))
